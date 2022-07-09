aff748cf9a14c0d3f8940aa385164cd4e170acc8
zyc <zycDeepin0909@zyc.com>
2022-07-09 13:52:05 +0800

    基础算法-二分查找-第一天

---
+++ leetcode/src/com/zyc/day0709/Solution1.java
@@ -0,0 +1,31 @@
+package com.zyc.day0709;
+
+/*
+ * 二分查找，
+ */
+public class Solution1 {
+
+	public static int search(int[] nums, int target) {
+		int left = 0, right = nums.length - 1;
+		int mid = (left + right) / 2;
+		while (left <= right) {
+			if (target < nums[mid]) {
+				right = mid - 1;
+			}
+			if(target > nums[mid]) {
+				left = mid + 1;
+			}
+			if(target == nums[mid])
+			{
+				return mid;
+			}
+			mid = (left + right) / 2;
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = new int[]{-1,0,3,5,9,12};
+		System.out.println(search(nums, 2));
+	}
+}
