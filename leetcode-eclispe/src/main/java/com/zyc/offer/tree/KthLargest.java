cd537f7eab45df9ce12b15a6ad98b0d52f6fbc6c
zyc <zyc@windows.com>
2022-09-09 10:03:18 +0800

    54.二叉搜索树的第K个大的节点

---
+++ leetcode-eclispe/src/main/java/com/zyc/offer/tree/KthLargest.java
@@ -0,0 +1,52 @@
+package com.zyc.offer.tree;
+
+/**
+ * @author ZYC
+ */
+public class KthLargest {
+
+    private int ret = 0;
+    private int count = 0;
+
+    /**
+     * 二叉搜索树第k个大的节点
+     * @param root 树根
+     * @param k 倒数第k个节点
+     * @return 返回倒数第k个结果
+     */
+    public int kthLargest(TreeNode<Integer> root, int k) {
+        inOrderTree(root, k);
+        return ret;
+    }
+
+    private void inOrderTree(TreeNode<Integer> treeNode, int k) {
+        if (treeNode==null) {
+            return;
+        }
+
+        inOrderTree(treeNode.right, k);
+
+        count++;
+        if (count == k) {
+            ret = treeNode.val;
+            return;
+        }
+
+        inOrderTree(treeNode.left, k);
+    }
+
+    public TreeNode<Integer> binInsert(TreeNode<Integer> treeNode, int val) {
+        if (treeNode==null) {
+            return new TreeNode<>(val);
+        } else if (treeNode.val==val) {
+            return treeNode;
+        } else if (treeNode.val > val) {
+            treeNode.left = binInsert(treeNode.left, val);
+            return treeNode;
+        } else {
+            treeNode.right = binInsert(treeNode.right, val);
+            return treeNode;
+        }
+    }
+
+}
---
+++ leetcode-eclispe/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+module leetcode.eclispe {
+    uses Arrays;
+    uses Map;
+    uses List;
+    uses Queue;
+}
